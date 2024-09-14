3543fc7ef0c3aae56c6efc54c3230a03b4810fe3
Rastir <aldojero@hotmail.com>
2024-09-13 21:54:23 -0500

    vacas

---
+++ cala/src/main/java/com/liveaqua/cala/model/vacas.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.liveaqua.cala.model;
+
+/**
+ *
+ * @author aldoj
+ */
+public class vacas {
+    
+    String namr;
+    
+}
