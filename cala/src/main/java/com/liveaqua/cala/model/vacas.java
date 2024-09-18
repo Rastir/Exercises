1f6619124aa28ca58a6e1e2f68f58e7ed17cfb5f
Rastir <aldojero@hotmail.com>
2024-09-17 23:18:19 -0500

    vacas

--- cala/src/main/java/com/liveaqua/cala/model/vacas.java
+++ cala/src/main/java/com/liveaqua/cala/model/vacas.java
@@ -12,5 +12,8 @@ public class vacas {
     
     String namr;
     // esto es un comentario diciendo que estas son mis vacaciones dia 1 
+    // esto es un comentario diciendo que éstas son mis vacaciones, dia 2, fui de pesca con mi papá y mi esposa
+    // me la pasé increible, sacamos 5 lobinas y 2 truchas arcoiris, ha sido el mejor día de mi vida 17 de sep 2024,
+    // a un día de cumplir 30 :)
     
 }
