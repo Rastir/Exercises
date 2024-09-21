4bc5a3ac893345f6fb6692674a6e0a64519a8ccf
Rastir <aldojero@hotmail.com>
2024-09-21 00:01:21 -0500

    vacas

--- cala/src/main/java/com/liveaqua/cala/model/vacas.java
+++ cala/src/main/java/com/liveaqua/cala/model/vacas.java
@@ -22,5 +22,10 @@ public class vacas {
     // Y MIS ABUELOS, HEMOS VISITADO DOLORES HIDALGO Y COMIDO HELADO DE LAVANDA, HA ESTADO DELICIOSO, MAÑANA ESPERA
     // UN DÍA DE SPA EN LAS AGUAS TERMALES "EL ESCONDIDO" DESPUES UNA VISITA AL CENTRO HISTORICO Y DESPUES UNA VUELTA
     // AL SUPERMERCADO CON LA FAMILIA.
+    // Esto es un comentario diciendo que éstas son mis vacaciones, día 5, el día de hoy fui a un balneario con mi 
+    // familia, en especifico el "escondido" que despues de tantos años sigue siendo lo que recordaba de niño, solo que
+    // ahora de adulto lo puedo apreciar más, es hermoso, no habia tenido un momento de relajación así en un buen tiempo
+    // agradezco a mi Dios por mi familia y la oportunidad de disfrutar el momento que tengo con ellos mientras pueda
+    // solo mi señor sabe cuanto pueda disfrutar de esto.
     
 }
