8580b230dc091fda6185b2846963674c48937638
Rastir <aldojero@hotmail.com>
2024-09-25 16:56:41 -0500

    digital exercise archive

--- clin_vet-master/clinica_veterinaria/src/main/java/com/todocodeacademy/clinica_veterinaria/ClinicaVeterinariaApplication.java
+++ clin_vet-master/clinica_veterinaria/src/main/java/com/todocodeacademy/clinica_veterinaria/ClinicaVeterinariaApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClinicaVeterinariaApplication {
+public class
+
+
+ClinicaVeterinariaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaVeterinariaApplication.class, args);
