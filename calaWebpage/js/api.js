b0cfdab64c71e3bfa8f0f0b9550bea45c4bb0479
Rastir <aldojero@hotmail.com>
2024-11-12 16:19:14 -0500

    moved

--- calaWebpage/js/api.js
+++
@@ -1,7 +0,0 @@
-// url frl spi
-const apiUrl ="http://vps-4437505-x.dattaweb.com:8080/personas/traer"
-
-fetch(apiUrl)
-.then(Response => Response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error))
