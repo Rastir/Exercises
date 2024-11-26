c784a3a90acb85919025a3d7a423aaa447bc9ea8
Rastir <aldojero@hotmail.com>
2024-11-25 22:33:20 -0500

    finished the problems with circuit brakers, it was a version of springboot problem

---
+++ Microservicios con Java/patrones de diseño/circuit breaker/cities-service(aldo)/src/main/java/com/aldosanchez/cities_service/model/City.java
@@ -0,0 +1,19 @@
+package com.aldosanchez.cities_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class City {
+
+    private Long city_id;
+    private String name;
+    private String continent;
+    private String country;
+    private String state;
+
+}
---
+++ Microservicios con Java/patrones de diseño/circuit breaker/cities-service(aldo)/src/main/java/com/aldosanchez/cities_service/repository/IHotelsAPI.java
@@ -0,0 +1,19 @@
+package com.aldosanchez.cities_service.repository;
+
+import com.aldosanchez.cities_service.dto.HotelDTO;
+import lombok.Getter;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+//mismo nombre de la app en eureka
+//asi ya no es necessario configurar la url
+@FeignClient(name = "hotels-service")
+public interface IHotelsAPI {
+
+    @GetMapping("hotels/{city_id}")
+    public List<HotelDTO> getHotelsByCityId (@PathVariable("city_id") Long city_id);
+
+}
